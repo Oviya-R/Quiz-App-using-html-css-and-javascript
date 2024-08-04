375e554b75cbbc9ea9b9dc80b16c85104d2539b8
Oviya-R <oviya.ravichandran2004@gmail.com>
2024-08-04 23:22:14 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,88 @@
+function text(){
+    document.getElementById("green01").style.backgroundColor="green";
+    document.getElementById("green1").style.backgroundColor="green";
+}
+function text1(){
+    document.getElementById("red01").style.backgroundColor="red";
+    document.getElementById("red1").style.backgroundColor="red";
+}
+function text2(){
+    document.getElementById("red02").style.backgroundColor="red";
+    document.getElementById("red2").style.backgroundColor="red";
+}
+function text3(){
+    document.getElementById("red03").style.backgroundColor="red";
+    document.getElementById("red3").style.backgroundColor="red";
+}
+
+
+function text01(){
+    document.getElementById("green02").style.backgroundColor="green";
+    document.getElementById("green2").style.backgroundColor="green";
+}
+function text4(){
+    document.getElementById("red04").style.backgroundColor="red";
+    document.getElementById("red4").style.backgroundColor="red";
+}
+function text5(){
+    document.getElementById("red05").style.backgroundColor="red";
+    document.getElementById("red5").style.backgroundColor="red";
+}
+function text6(){
+    document.getElementById("red06").style.backgroundColor="red";
+    document.getElementById("red6").style.backgroundColor="red";
+}
+
+
+function text02(){
+    document.getElementById("green03").style.backgroundColor="green";
+    document.getElementById("green3").style.backgroundColor="green";
+}
+function text7(){
+    document.getElementById("red07").style.backgroundColor="red";
+    document.getElementById("red7").style.backgroundColor="red";
+}
+function text8(){
+    document.getElementById("red08").style.backgroundColor="red";
+    document.getElementById("red8").style.backgroundColor="red";
+}
+function text9(){
+    document.getElementById("red09").style.backgroundColor="red";
+    document.getElementById("red9").style.backgroundColor="red";
+}
+
+
+function text03(){
+    document.getElementById("green04").style.backgroundColor="green";
+    document.getElementById("green4").style.backgroundColor="green";
+}
+function text10(){
+    document.getElementById("red010").style.backgroundColor="red";
+    document.getElementById("red10").style.backgroundColor="red";
+}
+function text11(){
+    document.getElementById("red011").style.backgroundColor="red";
+    document.getElementById("red11").style.backgroundColor="red";
+}
+function text12(){
+    document.getElementById("red012").style.backgroundColor="red";
+    document.getElementById("red12").style.backgroundColor="red";
+}
+
+
+function text04(){
+    document.getElementById("green05").style.backgroundColor="green";
+    document.getElementById("green5").style.backgroundColor="green";
+}
+function text13(){
+    document.getElementById("red013").style.backgroundColor="red";
+    document.getElementById("red13").style.backgroundColor="red";
+}
+function text14(){
+    document.getElementById("red014").style.backgroundColor="red";
+    document.getElementById("red14").style.backgroundColor="red";
+}
+function text15(){
+    document.getElementById("red015").style.backgroundColor="red";
+    document.getElementById("red15").style.backgroundColor="red";
+}
